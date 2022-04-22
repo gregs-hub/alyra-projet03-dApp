049c8e3805c19bf6909d4aaaa71733228648cee4
gregs-hub <gseiller@hotmail.com>
2022-04-22 21:08:25 +0200

    Updates

---
+++ precache-manifest.98f0dece09f34441397c66c114c864ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d275eb346fe9bb22ec623da5526fc5",
+    "url": "/alyra-projet03-dApp/index.html"
+  },
+  {
+    "revision": "ec6955dd27c355ddbfb9",
+    "url": "/alyra-projet03-dApp/static/css/2.37c457eb.chunk.css"
+  },
+  {
+    "revision": "08608bbd8014fb073236",
+    "url": "/alyra-projet03-dApp/static/css/main.0aadf677.chunk.css"
+  },
+  {
+    "revision": "ec6955dd27c355ddbfb9",
+    "url": "/alyra-projet03-dApp/static/js/2.a38246a7.chunk.js"
+  },
+  {
+    "revision": "08608bbd8014fb073236",
+    "url": "/alyra-projet03-dApp/static/js/main.486435fa.chunk.js"
+  },
+  {
+    "revision": "639f4471dbfd630aa0ed",
+    "url": "/alyra-projet03-dApp/static/js/runtime-main.84cc5260.js"
+  },
+  {
+    "revision": "77827aa5a39e3fa55e0079907e278a17",
+    "url": "/alyra-projet03-dApp/static/media/logo_alary.77827aa5.png"
+  }
+]);
